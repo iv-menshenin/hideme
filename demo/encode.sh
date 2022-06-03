2543175df02f0bed3eee5662f58d9fd259bc5065
devalio <github@devalio.ru>
2022-06-03 15:01:03 +0300

    Fixed demo

---
+++ demo/encode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./hideme inject --payload=./original.jpg --carrier=./carrier.png --out=./encoded.png --private=./rsa_key --encode-key=./crypt-key.jpg --aes-key=af012453af01245305f76a0005f76a00
+
